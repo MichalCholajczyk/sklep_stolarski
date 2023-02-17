2fcde6b64ce6bd2d520fc0b59700ced70e2381ee
MichalCholajczyk <michalcholajczyk@gmail.com>
2023-02-17 20:25:48 +0100

    added login and logout after clicking on profile

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useRef, useEffect } from "react";
 
-import { NavLink, useNavigate } from "react-router-dom";
+import { NavLink, useNavigate, Link } from "react-router-dom";
 import "./header.css";
 
 import { motion } from "framer-motion";
@@ -10,9 +10,10 @@ import userIcon from "../../assets/images/user-icon.png";
 
 import { Container, Row } from "reactstrap";
 import { useSelector } from "react-redux";
-import useAuth from "../../custom-hooks/useAuth"
-
-
+import useAuth from "../../custom-hooks/useAuth";
+import { signOut } from "firebase/auth";
+import { auth } from "../../firebase.config";
+import { toast } from "react-toastify";
 
 const nav__links = [
 	{
@@ -30,44 +31,52 @@ const nav__links = [
 	{
 		path: "login",
 		display: "Login",
-	}
+	},
 ];
 
 const Header = () => {
-	const headerRef = useRef(null)
+	const headerRef = useRef(null);
 
-	const menuRef = useRef(null)
-	const navigate = useNavigate()
-	const {currentUser} = useAuth()
+	const menuRef = useRef(null);
+	const navigate = useNavigate();
+	const { currentUser } = useAuth();
 
-	const totalQuantity = useSelector(state => state.cart.totalQuantity)
-	
+	const totalQuantity = useSelector((state) => state.cart.totalQuantity);
+
+	const profileActionRef = useRef(null);
 
 	const stickyHeaderFunction = () => {
-		window.addEventListener('scroll', () => {
-			if (
-				document.body.scrollTo > 80 ||
-				document.documentElement.scrollTop > 80
-			) {
-				headerRef.current.classList.add('sticky__header')
+		window.addEventListener("scroll", () => {
+			if (document.body.scrollTo > 80 || document.documentElement.scrollTop > 80) {
+				headerRef.current.classList.add("sticky__header");
 			} else {
-				headerRef.current.classList.remove('sticky__header')
+				headerRef.current.classList.remove("sticky__header");
 			}
-		})
-	}
+		});
+	};
+
+const logout =()=>{
+	signOut(auth).then(()=>{
+		toast.success('logged out successfully')
+		navigate("/home")
+	}).catch(err=>{
+		toast.error(err.message)
+	})
+}
 
 	useEffect(() => {
-		stickyHeaderFunction()
+		stickyHeaderFunction();
 
-		return () => window.removeEventListener("scroll", stickyHeaderFunction)
-	})
+		return () => window.removeEventListener("scroll", stickyHeaderFunction);
+	});
 
-	const menuToggle = () => menuRef.current.classList.toggle('active__menu')
+	const menuToggle = () => menuRef.current.classList.toggle("active__menu");
 
-	const navigateToCart = ()=>{
-		navigate("/cart")
-	}
+	const navigateToCart = () => {
+		navigate("/cart");
+	};
 
+	const toggleProfileActions = () => profileActionRef.current.classList.toggle("show__profileActions")
 
 	return (
 		<header className="header" ref={headerRef}>
@@ -103,16 +112,30 @@ const Header = () => {
 								<span className="badge">{totalQuantity}</span>
 							</span>
 
-							<span>
-								<motion.img className="user__Img" whileTap={{ scale: 1.2 }} src={currentUser? currentUser.photoURL: userIcon} alt="chuj" />
-							</span>
+							<div className="profile" >
+								<motion.img whileTap={{ scale: 1.2 }} src={currentUser ? currentUser.photoURL : userIcon} alt="chuj" 
+								onClick={toggleProfileActions}
+								/>
+
+								<div className="profile__actions" 
+								ref={profileActionRef} 
+								onClick={toggleProfileActions}>
+									{currentUser ? (
+										<span onClick={logout}>Logout</span>
+									) : (
+										<div className="d-flex align-items-center justify-content-center flex-column">
+											<Link to="/signup">Signup</Link>
+											<Link to="/login">Login</Link>
+										</div>
+									)}
+								</div>
+							</div>
 							<div className="mobile__menu">
 								<span onClick={menuToggle}>
 									<i class="ri-menu-line"></i>
 								</span>
 							</div>
 						</div>
-
 					</div>
 				</Row>
 			</Container>
